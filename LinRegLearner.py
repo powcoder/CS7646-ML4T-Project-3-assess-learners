b221d4d9bea7a9930d4e3e264ebe0fe1caf10947
powcoder <powcoder@163.com>
2023-09-22 23:07:40 +0800

    CS7646 ML4T Project 3 assess learners

---
+++ LinRegLearner.py
@@ -0,0 +1,64 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+	  		 		  		  		    	 		 		   		 		  
+import numpy as np  		  	   		  		 		  		  		    	 		 		   		 		  
+  		  	   		  		 		  		  		    	 		 		   		 		  
+  		  	   		  		 		  		  		    	 		 		   		 		  
+class LinRegLearner(object):  		  	   		  		 		  		  		    	 		 		   		 		  
+    """  		  	   		  		 		  		  		    	 		 		   		 		  
+    This is a Linear Regression Learner. It is implemented correctly.  		  	   		  		 		  		  		    	 		 		   		 		  
+  		  	   		  		 		  		  		    	 		 		   		 		  
+    :param verbose: If “verbose” is True, your code can print out information for debugging.  		  	   		  		 		  		  		    	 		 		   		 		  
+        If verbose = False your code should not generate ANY output. When we test your code, verbose will be False.  		  	   		  		 		  		  		    	 		 		   		 		  
+    :type verbose: bool  		  	   		  		 		  		  		    	 		 		   		 		  
+    """  		  	   		  		 		  		  		    	 		 		   		 		  
+    def __init__(self, verbose=False):  		  	   		  		 		  		  		    	 		 		   		 		  
+        """  		  	   		  		 		  		  		    	 		 		   		 		  
+        Constructor method  		  	   		  		 		  		  		    	 		 		   		 		  
+        """  		  	   		  		 		  		  		    	 		 		   		 		  
+        pass  # move along, these aren't the drones you're looking for  		  	   		  		 		  		  		    	 		 		   		 		  
+  		  	   		  		 		  		  		    	 		 		   		 		  
+    def author(self):  		  	   		  		 		  		  		    	 		 		   		 		  
+        """  		  	   		  		 		  		  		    	 		 		   		 		  
+        :return: The GT username of the student  		  	   		  		 		  		  		    	 		 		   		 		  
+        :rtype: str  		  	   		  		 		  		  		    	 		 		   		 		  
+        """  		  	   		  		 		  		  		    	 		 		   		 		  
+        return "tb34"  # replace tb34 with your Georgia Tech username  		  	   		  		 		  		  		    	 		 		   		 		  
+  		  	   		  		 		  		  		    	 		 		   		 		  
+    def add_evidence(self, data_x, data_y):  		  	   		  		 		  		  		    	 		 		   		 		  
+        """  		  	   		  		 		  		  		    	 		 		   		 		  
+        Add training data to learner  		  	   		  		 		  		  		    	 		 		   		 		  
+  		  	   		  		 		  		  		    	 		 		   		 		  
+        :param data_x: A set of feature values used to train the learner  		  	   		  		 		  		  		    	 		 		   		 		  
+        :type data_x: numpy.ndarray  		  	   		  		 		  		  		    	 		 		   		 		  
+        :param data_y: The value we are attempting to predict given the X data  		  	   		  		 		  		  		    	 		 		   		 		  
+        :type data_y: numpy.ndarray  		  	   		  		 		  		  		    	 		 		   		 		  
+        """  		  	   		  		 		  		  		    	 		 		   		 		  
+  		  	   		  		 		  		  		    	 		 		   		 		  
+        # slap on 1s column so linear regression finds a constant term  		  	   		  		 		  		  		    	 		 		   		 		  
+        new_data_x = np.ones([data_x.shape[0], data_x.shape[1] + 1])  		  	   		  		 		  		  		    	 		 		   		 		  
+        new_data_x[:, 0 : data_x.shape[1]] = data_x  		  	   		  		 		  		  		    	 		 		   		 		  
+  		  	   		  		 		  		  		    	 		 		   		 		  
+        # build and save the model  		  	   		  		 		  		  		    	 		 		   		 		  
+        self.model_coefs, residuals, rank, s = np.linalg.lstsq(  		  	   		  		 		  		  		    	 		 		   		 		  
+            new_data_x, data_y, rcond=None  		  	   		  		 		  		  		    	 		 		   		 		  
+        )  		  	   		  		 		  		  		    	 		 		   		 		  
+  		  	   		  		 		  		  		    	 		 		   		 		  
+    def query(self, points):  		  	   		  		 		  		  		    	 		 		   		 		  
+        """  		  	   		  		 		  		  		    	 		 		   		 		  
+        Estimate a set of test points given the model we built.  		  	   		  		 		  		  		    	 		 		   		 		  
+  		  	   		  		 		  		  		    	 		 		   		 		  
+        :param points: A numpy array with each row corresponding to a specific query.  		  	   		  		 		  		  		    	 		 		   		 		  
+        :type points: numpy.ndarray  		  	   		  		 		  		  		    	 		 		   		 		  
+        :return: The predicted result of the input data according to the trained model  		  	   		  		 		  		  		    	 		 		   		 		  
+        :rtype: numpy.ndarray  		  	   		  		 		  		  		    	 		 		   		 		  
+        """  		  	   		  		 		  		  		    	 		 		   		 		  
+        return (self.model_coefs[:-1] * points).sum(axis=1) + self.model_coefs[  		  	   		  		 		  		  		    	 		 		   		 		  
+            -1  		  	   		  		 		  		  		    	 		 		   		 		  
+        ]  		  	   		  		 		  		  		    	 		 		   		 		  
+  		  	   		  		 		  		  		    	 		 		   		 		  
+  		  	   		  		 		  		  		    	 		 		   		 		  
+if __name__ == "__main__":  		  	   		  		 		  		  		    	 		 		   		 		  
+    print("the secret clue is 'zzyzx'")  		  	   		  		 		  		  		    	 		 		   		 		  
---
+++ README.md
@@ -0,0 +1,9 @@
+# CS7646 ML4T Project 3 assess learners
+# 加微信 powcoder
+
+# QQ 1823890830
+
+# Programming Help Add Wechat powcoder
+
+# Email: powcoder@163.com
+
